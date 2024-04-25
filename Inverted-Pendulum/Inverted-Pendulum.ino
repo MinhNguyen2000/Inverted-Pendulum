8145067e11b72f3fbbadfe34d2a6c0ce2e889718
Minh Nguyen <caominh11122000@gmail.com>
2024-04-25 15:41:34 -0300

    ISR for encoder read
    Implemented interrupt service routine for reading the encoder values

--- Inverted-Pendulum/Inverted-Pendulum.ino
+++ Inverted-Pendulum/Inverted-Pendulum.ino
@@ -1,22 +1,27 @@
 // Pin definitions
-#define PENDULUM_ENCODER_A 2
-#define PENDULUM_ENCODER_B 3
+volatile int pendEncoderA_pin = 2;
+volatile int pendEncoderB_pin = 3;
 
 // Constants definitions
 
 // Variable definitions
-int pendEncoderA_stateLast, pendEncoderA_state, pendEncoderB_state;
-int counter = 0;
+volatile int pendEncoderA_stateLast, pendEncoderA_state, pendEncoderB_state;
+volatile long counter = 0;
 int lookup_table[] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
 int oldState, newState;
+volatile bool readState = false;
+
+void encoderInterrupt() {
+  readState = true;
+}
 
 void setup() {
-  pinMode(PENDULUM_ENCODER_A, INPUT_PULLUP);
-  pinMode(PENDULUM_ENCODER_B, INPUT_PULLUP);
-  attachInterrupt(digitalPintoInterrupt(PENDULUM_ENCODER_A), void (*userFunc)(void), int mode)
+  pinMode(pendEncoderA_pin, INPUT_PULLUP);
+  pinMode(pendEncoderB_pin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pendEncoderA_pin), encoderInterrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(pendEncoderB_pin), encoderInterrupt, CHANGE);
 
   Serial.begin(115200);
-  pendEncoderA_stateLast = digitalRead(PENDULUM_ENCODER_A);
 }
 
 void loop() {
@@ -24,23 +29,13 @@ void loop() {
   // Serial.println(digitalRead(PENDULUM_ENCODER_B));
 
   // =============== READ THE SENSOR DATA =============== //
-  pendEncoderA_state = digitalRead(PENDULUM_ENCODER_A); // Reads the "current" state of the outputA
-  pendEncoderB_state = digitalRead(PENDULUM_ENCODER_B); // Reads the "current" state of the outputB
-  newState = pendEncoderB_state * 2 + pendEncoderA_state; // State ranging from 0 to 3 for the quadrature encoderr reading
+  if (readState) {
+    reading = 
+    newState = pendEncoderB_state * 2 + pendEncoderA_state; // State ranging from 0 to 3 for the quadrature encoderr reading
 
+    readState = false;
+  }
   // =============== PROCESS THE SENSOR DATA =============== //
-  // // If the previous and the current state of the outputA are different, that means a Pulse has occured
-  // if (pendEncoderA_state != pendEncoderA_stateLast){     
-  //   // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-  //   if (pendEncoderB_state != pendEncoderA_state) { 
-  //     counter = counter + 1;
-  //   } else {
-  //     counter = counter - 1;
-  //   }
-  //   Serial.print("Position:");
-  //   Serial.println(counter);
-  // } 
-  // pendEncoderA_stateLast = pendEncoderA_state; // Updates the previous state of the outputA with the current state
   counter = counter + lookup_table[oldState * 4 + newState];
   
 
