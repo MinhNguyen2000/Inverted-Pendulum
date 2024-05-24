ebe4a3c5dcdd3fe8c2e3b94496d461b5358e96d8
Minh Nguyen <caominh11122000@gmail.com>
2024-05-24 16:11:13 -0300

    Pole placement + simulation

--- MATLAB/cartpend.m
+++ MATLAB/cartpend.m
@@ -7,4 +7,4 @@ den = M + m * Sy^2;
 dy(1, 1) = y(2);
 dy(2, 1) = (m * Sy * (L * y(4)^2 - g * Cy) - d * y(2) + u) / den;
 dy(3, 1) = y(4);
-dy(4, 1) = ((M + m) * g * Sy - m * L * y(4)^2 * Sy * Cy + d * y(2)* Cy + Cy * u)/(L * den);
+dy(4, 1) = ((M + m) * g * Sy - m * L * y(4)^2 * Sy * Cy + d * y(2)* Cy - Cy * u)/(L * den);
