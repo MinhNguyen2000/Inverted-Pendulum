abd389c8d35ce79f63d0d3112a35d21f6494aa9c
Minh Nguyen <caominh11122000@gmail.com>
2024-05-23 23:33:13 -0300

    MATLAB invpen response plots

--- MATLAB/sim_cartpend.m
+++ MATLAB/sim_cartpend.m
@@ -8,10 +8,24 @@ L = 2;
 g = -10;
 d = 1;
 
+u = 0; % No control input (free response)
+
 tspan = 0:0.1:10;
 y0 = [0; 0; pi; .5];
-[t,y] = ode45(@(t,y)cartpend(y,m,M,L,g,d,0), tspan, y0);
+[t,y] = ode45(@(t,y)cartpend(y,m,M,L,g,d,u), tspan, y0);
 
 for k = 1:length(t)
     drawcartpend_bw(y(k,:),m,M,L);
 end
+
+title_list = ["$x$", "$\dot x$", "$\theta$", "$\dot \theta$"];
+figure()
+for i = 1:4
+    subplot(4,1,i)
+    plot(t,y(:,i))
+    title(title_list(i),'interpreter','latex')
+    grid minor
+end
+xlabel('Time (s)')
+
+% figure(title = 'Control input')
